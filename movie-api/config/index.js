134964a29e1c69d36298a437e34bd5c4d4554c36
K-LoZ3 <carlos193zambra@gmail.com>
2021-05-11 11:51:11 -0500

    Creamos varios usuarios, agregamos mas peliculas y creamos las key-tokens para darles permisos a los usuarios y admins dependiendo de que token tengan. Esto lo hicimos con scripts para facilitar subir esos archivos a la base de datos.

--- movie-api/config/index.js
+++ movie-api/config/index.js
@@ -11,6 +11,11 @@ const config = {
   dbPassword: process.env.DB_PASSWORD,
   dbHost: process.env.DB_HOST,
   dbName: process.env.DB_NAME,
+  defaultAdminPassword: process.env.DEFAULT_ADMIN_PASSWORD,
+  defaultUserPassword: process.env.DEFAULT_USER_PASSWORD,
+  authJWTSecret: process.env.AUTH_JWT_SECRET,
+  publuctApiKeyToken: process.env.PUBLIC_API_KEY_TOKEN,
+  adminApiKeyToken: process.env.ADMIN_API_KEY_TOKEN,
 }
 
 module.exports = { config };
