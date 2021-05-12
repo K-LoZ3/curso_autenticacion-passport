8721c81c49209c30908a7ed26b697f0d71031edf
K-LoZ3 <carlos193zambra@gmail.com>
2021-05-12 16:09:32 -0500

    Implementamos la ruta para el sign-up. De esta manera podemos crear usuarios y almacenarlos en la base de datos.

--- movie-api/routes/auth.js
+++ movie-api/routes/auth.js
@@ -4,6 +4,11 @@ const boom = require('@hapi/boom'); // Para el manejo de errores.
 const jwt = require('jsonwebtoken'); // Para el manejo de jwt. Crearlo en este caso.
 const ApiKeysService = require('../services/apiKeys'); // Para buscar el alcance que tendra cada usuario
 // Esto porque en la base de datos tenemos los alcances que tendra cada tipo de user.
+const UsersService = require('../services/users'); // Para crear los usuarios.
+const validationHandler = require('../utils/middleware/validationHandler'); // Para validar que el objeto que pasemos para
+// crear el usuario concuerde con el schema.
+
+const { createUserSchema } = require('../utils/schemas/users');
 
 // Para obtener el secret con el que firmaremos el token.
 const { config } = require('../config');
@@ -23,6 +28,8 @@ function authApi(app) {
 
   // Instanciamos los servicios para buscar el scope que daremos al user.
   const apiKeysService = new ApiKeysService();
+  // Instanciamos los servicios para buscar y crear usuarios.
+  const usersService = new UsersService();
 
   // creamos el endpoint para hacer post del user. Esto para crear el jwt.
   router.post('/sign-in', async function(req, res, next) {
@@ -97,6 +104,26 @@ function authApi(app) {
     // Entonces debemos hacer un clousure con la firma de la ruta.
     // De esta manera nos aseguramos que el .authenticate y custom callback funcione sin problemas.
   });
+
+  // Creamos la ruta para crear usuarios.
+  // Validamos que lo que tiene el body si cumple con el schema para crear usuarios.
+  router.post('/sign-up', validationHandler(createUserSchema), async function(req, res, next) {
+    // Sacamos del body el usuario.
+    const { body: user } = req;
+
+    try {
+      // Creamos el usuario con los servicios de user.
+      const createdUserId = await usersService.createUser({ user });
+
+      // Devolvemos el userId que nos da mongoDB mas un mensaje.
+      res.status(201).json({
+        data: createdUserId,
+        message: 'user created',
+      })
+    } catch (error) {
+      next(error); // Manejamos el error.
+    }
+  });
 }
 
 module.exports = authApi;
@@ -125,5 +152,17 @@ module.exports = authApi;
     "name": "ROOT",
     "email": "root@undefined.sh"
   }
+
+  ---------------------------------------------
+  Si vamos postman y usamos la ruta: http://localhost:3000/api/auth/sign-up para crear
+  usuarios lo que devemos hacer es con el meto POST y en el body pasar el objeto json
+  con el usuario como esta en el schema. Un user nuevo quedaria asi:
+  {
+    "name": "carlos eduardo",
+    "email": "carlos@gmail.com",
+    "password": "secret"
+  }
+
+  Luego podemos ver si el usuario esta bien haciendo uso del sign-in.
 }
 */
